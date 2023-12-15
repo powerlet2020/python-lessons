25e7e08082b7b05e770ae05131aba6f919743f33
Hamza Sulemana <powerlet2020@gmail.com>
2023-12-15 15:12:42 +0000

    file updated

--- oop2.py
+++ oop2.py
@@ -20,3 +20,38 @@ print(student2.name)
 print(student2.marks)
 did_pass = student2.check_pass_fail()
 print(did_pass)
+
+# ----------------lessons2....................
+class Triangle:
+    def AreaOfTriangle(self):
+        return 0.5 * self.base * self.height
+    
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+triangle1 = Triangle(2, 5)
+area_of_triangle1 = triangle1.AreaOfTriangle()
+print(area_of_triangle1)
+
+# ..............lesson3.......................
+
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+    
+    def calculate_area(self):
+       area =  0.5 * self.base * self.height
+       return area
+
+def main():
+    base = float(input('Enter the base of the triangle:'))
+    height = float(input('Enter the height of triangle:'))
+
+    # create an instance of the triangle
+    my_triangle = Triangle(base, height)
+    area = my_triangle.calculate_area()
+    print(f'The area of the triangle is:{area}')
+if __name__ == "__main__":
+    main()
