af52f56bfbad4c3a41345926ee83375ab9d84882
powerlet <powerlet2020@gmail.com>
2024-02-26 04:09:50 -0800

    updated

---
+++ functions.py
@@ -0,0 +1,15 @@
+def hello(to):
+    print("hello " ,to)
+
+name = input('what is your name:')
+hello(name)
+
+
+def main():
+    x = int(input("whats x?"))
+    print("x squared is", square(x))
+def square(n):
+    return n * n
+#   you can use pow(n,2) to mean the power of 
+main()
+    
